f80c3cb780c79625ddf94491362b8e3b2672c188
rupalisam1999 <rupalisangale1999@gmail.com>
2022-03-05 15:33:22 +0530

    [Rupali]ADDED: Analyze MOod happy or sad

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class MoodAnalyzer
+    {
+        public string AnalyzeMood(string moodMessage)
+        {
+            if (moodMessage.ToLower().Contains("SAD"))
+            {
+                return "SAD";
+
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MoodAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MoodAnalyzer moodAnalyser = new MoodAnalyzer();
+            string result = moodAnalyser.AnalyzeMood("I am in SAD mood");
+            Console.WriteLine(result);
+        }
+    }
+}
